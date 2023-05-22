ee0e86e6fae5d11ad0ab31d9abdc908e49c13961
not1insight <not1insight@gmail.com>
2023-05-22 22:45:55 +0200

    Dynamic creation of user id session folder

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module kiedit
 
 go 1.20
+
+require github.com/satori/go.uuid v1.2.0
--- main.go
+++ main.go
@@ -3,14 +3,32 @@ package main
 import (
 	"bytes"
 	"log"
+	"os"
 	"os/exec"
+
+	uuid "github.com/satori/go.uuid"
 )
 
-func main() {
-	command := exec.Command("sh", "run.sh")
+func generateUserSessionID() string {
+	return uuid.NewV4().String()
+}
 
+func createSessionDirectory(sessionID string) error {
+	sessionDir := "./dist/" + sessionID
+
+	return os.Mkdir(sessionDir, 0755)
+}
+
+func main() {
+	userSessionId := generateUserSessionID()
+
+	command := exec.Command("sh", "run.sh")
 	var stderr bytes.Buffer
 	command.Stderr = &stderr
+
+	if err := createSessionDirectory(userSessionId); err != nil {
+		log.Fatal(err)
+	}
 
 	if err := command.Run(); err != nil {
 		log.Fatal(err)
