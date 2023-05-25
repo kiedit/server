a54e473d54e12fea1aa68beaddcb9a926fd727c3
not1insight <not1insight@gmail.com>
2023-05-25 19:56:50 +0200

    Refactor upload handler

--- handler/upload.go
+++ handler/upload.go
@@ -13,12 +13,27 @@ import (
 type UploadHandler struct {
 }
 
+func (self *UploadHandler) Upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("File Upload Endpoint Hit")
+
+	r.ParseMultipartForm(10 << 20)
+
+	currentUser.Init()
+
+	if err := uploadFile(w, r); err != nil {
+		fmt.Println("Error uploading file")
+		log.Fatal(err)
+	}
+
+	fmt.Fprintf(w, "Successfully Uploaded File\n")
+}
+
 var currentUser = new(user.User)
 
 func processFileSegmentation(inputFile string) error {
 	var splitVideoInput = media.SplitVideoInput{
 		InputFile:     inputFile,
-		Segment:       "10",
+		Segment:       "30",
 		OutputDirPath: currentUser.SessionDir + "/output%03d.mp4",
 	}
 
@@ -71,19 +86,4 @@ func uploadFile(w http.ResponseWriter, r *http.Request) error {
 	}
 
 	return nil
-}
-
-func (self *UploadHandler) Upload(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("File Upload Endpoint Hit")
-
-	r.ParseMultipartForm(10 << 20)
-
-	currentUser.Init()
-
-	if err := uploadFile(w, r); err != nil {
-		fmt.Println("Error uploading file")
-		log.Fatal(err)
-	}
-
-	fmt.Fprintf(w, "Successfully Uploaded File\n")
 }
